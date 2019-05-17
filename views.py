ff4f619c0d131e8888837acd4ba69ea37ab4ac1b
Mehdi Kahouadji <mehdi.kahouadji@gmail.com>
2019-05-17 12:09:48 +0200

    Update views.py

--- views.py
+++ views.py
@@ -1,7 +1,5 @@
 import datetime
 
-# from cfginfo.models import 
-
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect, Http404, StreamingHttpResponse
 from django.urls import reverse
